86ed56163ed164a5c75fb52bdf0d29d29f04b405
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-02-28 13:07:19 +0000

    backup

--- src/Subprotocols/ChunkBackup.java
+++ src/Subprotocols/ChunkBackup.java
@@ -1,10 +1,12 @@
 package Subprotocols;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 
 public class ChunkBackup {
-	private MulticastSocket mc;
-	private MulticastSocket mdb;
+	private MulticastSocket mc;//control socket
+	private MulticastSocket mdb;//data socket
 	private int serverID;
 	
 	public ChunkBackup (MulticastSocket mc, MulticastSocket mdb, int serverID) {
@@ -17,7 +19,14 @@ public class ChunkBackup {
 		
 	}
 	
-	public void store() {
+	public void store() throws IOException {
+		byte[] buf = new byte[256];
+		DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
+		mdb.receive(receivedPacket);
 		
+		//Fazer o parse de buf; ler ate ao primeiro CRLF para o header
+		String msg = new String(receivedPacket.getData());
+		msg = msg.trim();
+		String[] parts = msg.split(" ");
 	}
 }
