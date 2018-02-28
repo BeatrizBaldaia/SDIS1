51fd5602c24662d518a2c8f57e5c2014df168b4f
AnabelaSilva <up201506034@fe.up.pt>
2018-02-28 13:07:54 +0000

    Non_Initiator Peer

--- src/NonInitiator/Peer.java
+++ src/NonInitiator/Peer.java
@@ -3,8 +3,27 @@ package NonInitiator;
 public class Peer {
 
 	public static void main(String[] args) {
-		
+		if (args.length != 5) {
+			System.out.println("Usage: java Peer <id> <MCC_ip> <MCC_port> <MDB_ip> <MDB_port>");
+			return;
+		}
 
+		int peerID = Integer.parseInt(args[0]);
+
+		InetAddress mcc_ip = InetAddress.getByName(args[1]);
+		int mcc_port = Integer.parseInt(args[2]);
+
+		InetAddress mdb_ip = InetAddress.getByName(args[3]);
+		int mdb_port = Integer.parseInt(args[4]);
+
+		MulticastSocket mcc = new MulticastSocket(mcc_port);
+		mcc.joinGroup(mcc_ip);
+
+		MulticastSocket mdb = new MulticastSocket(mdb_port);
+		mdb.joinGroup(mdb_ip);
+
+		ChunkBackup protocol = new ChunkBackup(mcc, mdb, peerID);
+
+		protocol.store();
 	}
-
 }
