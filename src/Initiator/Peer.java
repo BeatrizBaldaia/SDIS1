e3b3ca3db48d62a3e94fb33fcaf2cc6dedaba70e
AnabelaSilva <up201506034@fe.up.pt>
2018-03-07 11:41:19 +0000

    initiator

--- src/Initiator/Peer.java
+++ src/Initiator/Peer.java
@@ -3,6 +3,31 @@ package Initiator;
 public class Peer {
 
 	public static void main(String[] args) {
-	}
+			if (args.length != 5) {
+				System.out.println("Usage: java Peer <id> <MCC_ip> <MCC_port> <MDB_ip> <MDB_port> <file> <replicationDegree>");
+				return;
+			}
+
+			int peerID = Integer.parseInt(args[0]);
+
+			InetAddress mcc_ip = InetAddress.getByName(args[1]);
+			int mcc_port = Integer.parseInt(args[2]);
+
+			InetAddress mdb_ip = InetAddress.getByName(args[3]);
+			int mdb_port = Integer.parseInt(args[4]);
+
+			MulticastSocket mcc = new MulticastSocket(mcc_port);
+			mcc.joinGroup(mcc_ip);
+
+			MulticastSocket mdb = new MulticastSocket(mdb_port);
+			mdb.joinGroup(mdb_ip);
+
+			String fileName = args[5];
+			int replicationDegree = Integer.parseInt(args[6]);
+
+			ChunkBackup protocol = new ChunkBackup(mcc, mdb, peerID);
+
+			protocol.send();
+		}
 
 }
