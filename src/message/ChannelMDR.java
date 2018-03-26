a8314ed59254ab94fad608bc0f75a6f257e0ee4e
AnabelaSilva <up201506034@fe.up.pt>
2018-03-26 17:21:36 +0100

    f

--- src/message/ChannelMDR.java
+++ src/message/ChannelMDR.java
@@ -15,112 +15,112 @@ public class ChannelMDR {
 private static ChannelMDR instance = null;
 	
 	private MulticastSocket socket;
-    private InetAddress address;
-    private int port;
-    private int myID;
+	private InetAddress address;
+	private int port;
+	private int myID;
 
-    public ChannelMDR() {
-    }
+	public ChannelMDR() {
+	}
 
 
-    public static ChannelMDR getInstance() {
-    	if(instance == null) {
-    		instance = new ChannelMDR();
-    	}
-    	return instance;
-    }
-    
-    public void createMulticastSocket(String addressStr, String portStr, int myID) {
-    	this.myID = myID;
-        try {
-            address = InetAddress.getByName(addressStr);
-        } catch (UnknownHostException e) {
-            System.err.println("Error parsing address: " + addressStr);
-            e.printStackTrace();
-        }
-        
-        port = Integer.parseInt(portStr);
+	public static ChannelMDR getInstance() {
+		if(instance == null) {
+			instance = new ChannelMDR();
+		}
+		return instance;
+	}
+	
+	public void createMulticastSocket(String addressStr, String portStr, int myID) {
+		this.myID = myID;
+		try {
+			address = InetAddress.getByName(addressStr);
+		} catch (UnknownHostException e) {
+			System.err.println("Error parsing address: " + addressStr);
+			e.printStackTrace();
+		}
+		
+		port = Integer.parseInt(portStr);
 
-        MulticastSocket socket = null;
-        try {
-            socket = new MulticastSocket(port);
-            socket.setTimeToLive(1);
-            socket.joinGroup(address);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		MulticastSocket socket = null;
+		try {
+			socket = new MulticastSocket(port);
+			socket.setTimeToLive(1);
+			socket.joinGroup(address);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        this.socket = socket;
-    }
-    
-    /**
-     * 
-     * @return the address of the multicast data channel socket
-     */
-    public InetAddress getAddress() { return address; }
-    
-    /**
-     * 
-     * @return the port of the multicast data channel socket
-     */
-    public int getPort() { return port; }
-    
+		this.socket = socket;
+	}
+	
+	/**
+	 * 
+	 * @return the address of the multicast data channel socket
+	 */
+	public InetAddress getAddress() { return address; }
+	
+	/**
+	* 
+	* @return the port of the multicast data channel socket
+*/
+	public int getPort() { return port; }
+	
 
-    /**
-     * Sends message.
-     *
-     * @param message to be sent.
-     */
-    public void sendMessage(byte[] message) {
-        DatagramPacket packet = new DatagramPacket(message, message.length, address, port);
+	/**
+	 * Sends message.
+	 *
+	 * @param message to be sent.
+	 */
+	public void sendMessage(byte[] message) {
+		DatagramPacket packet = new DatagramPacket(message, message.length, address, port);
 
-        try {
-            socket.send(packet);
-        } catch (IOException ignored) {
-        }
-    }
+		try {
+			socket.send(packet);
+		} catch (IOException ignored) {
+		}
+	}
 
-    /**
-     * Listens to the socket for messages.
-     * Block until receives the message
-     */
-    public void listen() {
-       new Thread(() -> {
-            while (true) {
-                byte[] buffer = new byte[200 + 64 * 1000];//header + body
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+	/**
+	 * Listens to the socket for messages.
+	 * Block until receives the message
+	 */
+	public void listen() {
+	   new Thread(() -> {
+			while (true) {
+				byte[] buffer = new byte[200 + 64 * 1000];//header + body
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-                try {
-                    socket.receive(packet);
-                    byte[] msg = packet.getData();
-                    Parser parser = new Parser(msg, packet.getLength());
-                	if(parser.parseHeader() != 0) {
-                		System.out.println("Error parsing the message");
-                	}
-                	System.err.println("Teste");
-                	if(parser.senderID != myID) {
+				try {
+					socket.receive(packet);
+					byte[] msg = packet.getData();
+					Parser parser = new Parser(msg, packet.getLength());
+					if(parser.parseHeader() != 0) {
+						System.out.println("Error parsing the message");
+					}
+					System.err.println("Teste");
+					if(parser.senderID != myID) {
 
-                    	System.err.println("Teste");
-                		if(parser.messageType.equals("CHUNK")) {
+						System.err.println("Teste");
+						if(parser.messageType.equals("CHUNK")) {
 
-                        	System.err.println("Teste");
-                			System.out.println("Recived CHUNK message");
-                			LocalState.getInstance().notifyThatItWasSent(parser.fileName, parser.chunkNo);
-                			Path filepath = Peer.getP().resolve("restoreFile");
-                			if(Files.exists(filepath)) {
+							System.err.println("Teste");
+							System.out.println("Recived CHUNK message");
+							LocalState.getInstance().notifyThatItWasSent(parser.fileName, parser.chunkNo);
+							Path filepath = Peer.getP().resolve("restoreFile");
+							if(Files.exists(filepath)) {
 
-                            	System.err.println("Teste");
-                				Peer.restoreChunk(parser);
-                			}
-                		}
-                	}
-                	
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return;
-                }
-            }
-        }).start();
-    	
-    }
+								System.err.println("Teste");
+								Peer.restoreChunk(parser);
+							}
+						}
+					}
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+					return;
+				}
+			}
+		}).start();
+		
+	}
 }
