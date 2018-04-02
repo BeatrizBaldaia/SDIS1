75ab9f448e502cd824c05eec3e6f934be5318529
AnabelaSilva <up201506034@fe.up.pt>
2018-04-02 14:36:03 +0100

    script

--- script.sh
+++ script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+echo "Compiling...!"
 javac -d bin/ -cp src src/initiator/Peer.java
 javac -d bin/ -cp src src/client/TestApp.java
+
 cd bin
+echo "Creating RMI registry!"
 #rmiregistry &
 cd ..
-gnome-terminal -x java -cp bin initiator.Peer 1 1 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
-gnome-terminal -x java -cp bin initiator.Peer 1 2 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
-gnome-terminal -x java -cp bin initiator.Peer 1 3 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
+
+echo "Creating peers!"
+for (( c=1; c<=5; c++ ))
+do  
+	gnome-terminal -x java -cp bin initiator.Peer 1 $c localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
+done
