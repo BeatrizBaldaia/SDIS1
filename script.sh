f7cb153cda0025d8fba369b354929a6afa43b4d8
AnabelaSilva <up201506034@fe.up.pt>
2018-04-02 10:32:45 +0100

    a

--- script.sh
+++ script.sh
@@ -5,6 +5,6 @@ javac -d bin/ -cp src src/client/TestApp.java
 cd bin
 #rmiregistry &
 cd ..
-gnome-terminal -x java -cp bin initiator.Peer 1 1 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082 &
-gnome-terminal -x java -cp bin initiator.Peer 1 2 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082 &
-gnome-terminal -x java -cp bin initiator.Peer 1 3 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082 &
+gnome-terminal -x java -cp bin initiator.Peer 1 1 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
+gnome-terminal -x java -cp bin initiator.Peer 1 2 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
+gnome-terminal -x java -cp bin initiator.Peer 1 3 localhost:1099 226.0.0.1 8080 226.0.0.2 8081 226.0.0.0 8082
